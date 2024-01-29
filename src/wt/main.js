b901c92e2c0a218b382831dd1896f7eaa301a957
Evgeniy Panin <panin@tochka.com>
2024-01-29 21:57:29 +0100

    wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { join } from 'path';
+import { createPathVariables } from '../utils/createPathVariables.js';
+
+const { __dirname } = createPathVariables(import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerFile = join(__dirname, 'worker.js');
+    const workerThreads = cpus().map((_, i) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerFile);
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0)
+                    reject(new Error());
+            });
+            worker.postMessage(i + 10);
+        })
+    });
+    const workersResult = await Promise.allSettled(workerThreads);
+    const data = workersResult.map(({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: value || null,
+    }))
+    console.log(data);
 };
 
 await performCalculations();
