1b68c441a36d918597bc08b25c600635389247b2
Evgeniy Panin <panin@tochka.com>
2024-01-29 19:34:58 +0100

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = {};
+    process.argv.forEach((arg, i, arr) => {
+        if (arg.startsWith('--')) {
+            let value;
+            if (arr[i + 1] && !arr[i + 1].startsWith('--')) {
+                value = arr[i + 1];
+            } else {
+                value = '';
+            }
+            if (value) {
+                args[arg] = value;
+            }
+        }
+    });
+    const result = Object.entries(args)
+        .map(([key, value]) => `${key} is ${value}`)
+        .join(', ')
+    console.log(result);
 };
 
 parseArgs();
